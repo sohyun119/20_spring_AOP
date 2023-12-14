af3d1b4696f5fa003442765dac6a2235c5642470
DESKTOP-0GQC1V5\19_web_LSH <sh-119@naver.com>
2023-12-14 20:34:48 +0900

    AOP 사용방법 공부

---
+++ src/main/java/com/spring/aop/Employee.java
@@ -0,0 +1,27 @@
+package com.spring.aop;
+
+import org.springframework.stereotype.Component;
+
+// 테스트
+@Component
+public class Employee {
+
+	public void work() {
+		System.out.println("직원의 일을 한다.");
+	}
+	
+	public void getWorkingTime() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getInfo(String title, int salary) {
+		
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/spring/aop/HomeController.java
@@ -0,0 +1,49 @@
+package com.spring.aop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	
+	//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private Boss boss;
+	
+	@Autowired
+	private Manager manager;
+	
+	@Autowired
+	private Employee employee;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		
+		boss.work();
+		manager.work();
+		employee.work();
+		
+		System.out.println("\n\n");
+		
+		boss.getWorkingTime();
+		manager.getWorkingTime();
+		employee.getWorkingTime();
+		
+		System.out.println("\n\n");
+		
+		boss.getInfo("사장", 1000);
+		manager.getInfo("관라자", 700);
+		employee.getInfo("직원", 300);
+		
+		
+		return "home";
+		
+	}
+	
+	
+	
+}
